9ebac27a20ddc541b5a2358e4988547e3dbd2157
Sharukh <buddy2196@hotmail.com>
2026-01-29 18:48:33 +0530

    api.js fixed

--- src/api.js
+++ src/api.js
@@ -1,20 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: "/api",
   // FIX: Removed 'headers' here so we don't force JSON on file uploads
 });
 
 // 1. Request Interceptor: Auto-attach Token
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
-      config.headers['x-auth-token'] = token;
+      config.headers["x-auth-token"] = token;
     }
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error),
 );
 
 // 2. Response Interceptor: Auto-Logout only on 401
@@ -22,12 +22,12 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response && error.response.status === 401) {
-      localStorage.removeItem('token');
-      localStorage.removeItem('user');
-      window.location.href = '/';
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      window.location.href = "/";
     }
     return Promise.reject(error);
-  }
+  },
 );
 
 export default api;
